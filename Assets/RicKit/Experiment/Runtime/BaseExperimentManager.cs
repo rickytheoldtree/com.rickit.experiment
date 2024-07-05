a54c5324e4cf9829c3921360d35cb6fe694edb4c
rickytheoldtree <rickyliyiqi@outlook.com>
2024-07-05 09:35:04 +0800

    v1.0.2

--- Assets/RicKit/Experiment/Runtime/BaseExperimentManager.cs
+++ Assets/RicKit/Experiment/Runtime/BaseExperimentManager.cs
@@ -6,15 +6,10 @@ namespace RicKit.Experiment
 {
     public interface IPrefs
     {
-        void SetInt(string key, int value);
-        void SetString(string key, string value);
-        int GetInt(string key);
-        string GetString(string key);
-        void Save();
     }
+
     public abstract class BaseExperimentManager
     {
-        protected abstract IPrefs Prefs { get; }
         private const string FirstTimeLogin = "ExperimentFirstTimeLogin";
         private const string VersionKey = "ExperimentVersion";
         private readonly Dictionary<string, BaseExperiment> experiments = new Dictionary<string, BaseExperiment>();
@@ -24,11 +19,11 @@ namespace RicKit.Experiment
         /// </summary>
         public void Init()
         {
-            SetGroup(Prefs.GetString(VersionKey) != Application.version, 
-                Prefs.GetInt(FirstTimeLogin) == 0);
-            Prefs.SetInt(FirstTimeLogin, 1);
-            Prefs.SetString(VersionKey, Application.version);
-            Prefs.Save();
+            SetGroup(GetString(VersionKey) != Application.version,
+                GetInt(FirstTimeLogin) == 0);
+            SetInt(FirstTimeLogin, 1);
+            SetString(VersionKey, Application.version);
+            Save();
         }
 
         private void SetGroup(bool firstTimeThisVersion, bool firstTimeLogin)
@@ -47,6 +42,7 @@ namespace RicKit.Experiment
                             exp.SetGroup();
                             SaveExperiment(exp);
                         }
+
                         break;
                     case ExperimentTargetUser.OldUser:
                         if (!firstTimeLogin && firstTimeThisVersion)
@@ -54,33 +50,49 @@ namespace RicKit.Experiment
                             exp.SetGroup();
                             SaveExperiment(exp);
                         }
+
                         break;
                 }
+
                 Debug.Log($"{experiment.Key}\n{exp}\n是否为实验目标：{exp.isTarget}\n分组：{exp.group}");
             }
         }
+
         private T LoadExperiment<T>() where T : BaseExperiment, new()
         {
             var exp = new T();
             var key = typeof(T).Name;
-            var data = Prefs.GetString(key);
-            return !string.IsNullOrEmpty(data) ? exp.FromString(data) as T: exp;
+            var data = GetString(key);
+            return !string.IsNullOrEmpty(data) ? exp.FromString(data) as T : exp;
         }
+
         private void SaveExperiment<T>(T exp) where T : BaseExperiment
         {
             var key = exp.GetType().Name;
-            Prefs.SetString(key, exp.ToString());
-            Prefs.Save();
+            SetString(key, exp.ToString());
+            Save();
         }
+
         protected void RegisterExperiment<T>() where T : BaseExperiment, new()
         {
             var exp = LoadExperiment<T>();
             experiments[typeof(T).Name] = exp;
         }
+
         public void DoExperiment<T>(Action<T> action) where T : BaseExperiment
         {
             var exp = experiments[typeof(T).Name];
             action(exp as T);
         }
+
+        #region Prefs
+
+        protected abstract void SetInt(string key, int value);
+        protected abstract void SetString(string key, string value);
+        protected abstract int GetInt(string key);
+        protected abstract string GetString(string key);
+        protected abstract void Save();
+
+        #endregion
     }
 }
